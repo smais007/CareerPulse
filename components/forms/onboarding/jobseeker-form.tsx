84c7e432d9f3c13e2ef77a4c8d1db54393c67b39
smais007 <smaisshawon@gmail.com>
2025-03-28 01:02:54 +0600

    complete job seeker profile

--- components/forms/onboarding/jobseeker-form.tsx
+++ components/forms/onboarding/jobseeker-form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { createJobSeeker } from "@/app/action";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -12,10 +15,12 @@ import { Textarea } from "@/components/ui/textarea";
 import { UploadDropzone } from "@/utils/uploadthing";
 import { jobSeekerSchema } from "@/utils/zod-schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { XIcon } from "lucide-react";
+import { Loader2, XIcon } from "lucide-react";
 import Image from "next/image";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import PDF from "@/public/pdf.png";
 
 export default function JobSeekerForm() {
   const form = useForm<z.infer<typeof jobSeekerSchema>>({
@@ -27,9 +32,24 @@ export default function JobSeekerForm() {
     },
   });
 
+  const [pending, setPending] = React.useState(false);
+
+  async function onSubmit(data: z.infer<typeof jobSeekerSchema>) {
+    try {
+      setPending(true);
+      await createJobSeeker(data);
+    } catch (error) {
+      if (error instanceof Error && error.message !== "NEXT_REDIRECT") {
+        console.log("Error creating jobseeker profile:", error);
+      }
+    } finally {
+      setPending(false);
+    }
+  }
+
   return (
     <Form {...form}>
-      <form className="space-y-6">
+      <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
           name="name"
@@ -67,12 +87,13 @@ export default function JobSeekerForm() {
                 {field.value ? (
                   <div className="relative w-fit">
                     <Image
-                      src={field.value}
+                      src={PDF}
                       alt="Logo"
                       width={100}
                       height={100}
                       className="rounded-lg"
                     />
+
                     <Button
                       type="button"
                       variant="destructive"
@@ -99,6 +120,19 @@ export default function JobSeekerForm() {
             </FormItem>
           )}
         />
+
+        <Button type="submit" disabled={pending}>
+          {pending ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              <span>Creating Profile</span>
+            </>
+          ) : (
+            <>
+              <span>Create Profile</span>
+            </>
+          )}
+        </Button>
       </form>
     </Form>
   );
