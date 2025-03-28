197b4110286ee37250a35bd45c29e65cd98ebf31
smais007 <smaisshawon@gmail.com>
2025-03-28 19:16:01 +0600

    create testimonial section

---
+++ app/(mainLayout)/post-job/page.tsx
@@ -0,0 +1,106 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import GoogleLogo from "@/public/google.png";
+import AppleLogo from "@/public/Apple.svg";
+import MetaLogo from "@/public/Meta.svg";
+import Samsung from "@/public/samsung.svg";
+import VercelLogo from "@/public/vercel.svg";
+import Image from "next/image";
+
+const companies = [
+  { id: 1, name: "Google", logo: GoogleLogo },
+  { id: 2, name: "Apple", logo: AppleLogo },
+  { id: 3, name: "Meta", logo: MetaLogo },
+  { id: 4, name: "Samsung", logo: Samsung },
+  { id: 5, name: "Vercel", logo: VercelLogo },
+];
+
+const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit Lorem, ipsum dolor.ur adipisicing elit. Quisquam, voluptatum.",
+    author: "John Doe",
+    company: "Google",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet lorem2 adipisicing elit. Quisquam, voluptatum.",
+    author: "John Doe",
+    company: "Google",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit aje ad      adipisicing elit. Quisquam, voluptatum.",
+    author: "John Doe",
+    company: "Google",
+  },
+];
+
+const stats = [
+  { id: 0, value: "12k+", label: "Monthly active job seekers" },
+  { id: 1, value: "48h", label: "Average time to hire" },
+  { id: 2, value: "100%", label: "Job seekers satisfaction rate" },
+  { id: 3, value: "500+", label: "Companies hiring regularly" },
+];
+
+export default function PostJobPage() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
+      <Card className="col-span-1 lg:col-span-2">
+        <CardHeader>
+          <CardTitle>Post Job</CardTitle>
+        </CardHeader>
+      </Card>
+      <div className="col-span-1">
+        <Card className="h-full">
+          <CardHeader>
+            <CardTitle>Trused by thousands of companies</CardTitle>
+            <CardDescription>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Possimus, quisquam?
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 gap-4">
+              {companies.map((company) => (
+                <div key={company.id}>
+                  <Image
+                    src={company.logo}
+                    alt={company.name}
+                    width={80}
+                    height={80}
+                    className="rounded-lg"
+                  />
+                </div>
+              ))}
+            </div>
+            <div>
+              {testimonials.map((testimonial) => (
+                <div key={testimonial.quote}>
+                  <p>{testimonial.quote}</p>
+                  <p>{testimonial.author}</p>
+                  <p>{testimonial.company}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="grid grid-cols-2">
+              {stats.map((stat) => (
+                <div key={stat.id}>
+                  <p>{stat.value}</p>
+                  <p>{stat.label}</p>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
