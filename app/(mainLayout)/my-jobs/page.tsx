2eae184ac9c07be2602d7fef7cd4f84eb4723cc9
smais007 <smaisshawon@gmail.com>
2025-04-03 02:54:37 +0600

    add job listing table

---
+++ app/(mainLayout)/my-jobs/page.tsx
@@ -0,0 +1,154 @@
+import { EmptyState } from "@/components/general/empty-state";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { prisma } from "@/lib/db";
+import { requireUser } from "@/utils/requireUser";
+import { CopyCheckIcon, MoreHorizontal, PenBox, Trash2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getJobs(userId: string) {
+  const data = await prisma.jobPost.findMany({
+    where: {
+      Company: {
+        userId: userId,
+      },
+    },
+    select: {
+      id: true,
+      jobTitle: true,
+      status: true,
+      createdAt: true,
+      Company: {
+        select: {
+          name: true,
+          logo: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
+export default async function MyJobsPage() {
+  const session = await requireUser();
+  const data = await getJobs(session?.id as string);
+  return (
+    <>
+      {data.length == 0 ? (
+        <EmptyState
+          title="No job postings"
+          href="/post-job"
+          buttonText="Post a job"
+          description="Be the first one to post a job"
+        />
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle> My Jobs</CardTitle>
+            <CardDescription>
+              Manage your job postings and applications
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Logo</TableHead>
+                  <TableHead>Company</TableHead>
+                  <TableHead>Job Title</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Created At</TableHead>
+                  <TableHead className="text-right">Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data.map((listing) => (
+                  <TableRow key={listing.id}>
+                    <TableCell>
+                      <Image
+                        src={listing.Company.logo}
+                        alt={listing.Company.name}
+                        width={40}
+                        height={40}
+                        className="rounded-lg"
+                      />
+                    </TableCell>
+                    <TableCell>{listing.Company.name}</TableCell>
+                    <TableCell>{listing.jobTitle}</TableCell>
+                    <TableCell>
+                      {listing.status.charAt(0).toUpperCase() +
+                        listing.status.slice(1).toLowerCase()}
+                    </TableCell>
+                    <TableCell>
+                      {listing.createdAt.toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric",
+                      })}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" size={"icon"}>
+                            <MoreHorizontal />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem asChild>
+                            <Link href={`/job/${listing.id}/edit`}>
+                              <PenBox className="size-4" /> <span>Edit</span>
+                            </Link>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem asChild>
+                            <Link href={`/job/${listing.id}/edit`}>
+                              <CopyCheckIcon className="size-4" />{" "}
+                              <span>Copy Job URL</span>
+                            </Link>
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem asChild>
+                            <Link href={`/job/${listing.id}/delete`}>
+                              <Trash2 className="size-4" /> <span>Delete</span>
+                            </Link>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+}
